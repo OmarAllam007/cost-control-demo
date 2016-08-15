9808d57092b5bec6f9970ea1cbbc368f8ad299c1
Hazem Mohamed <hazem.mohamed@alkifah.com>
2016-08-15 09:56:52 +0300

    Add local CSS assets

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,6 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+        .copy('node_modules/font-awesome/fonts', 'public/fonts/')
 });
