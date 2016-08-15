16a9a8b12fd208bd9bc03779699e6c923fc79794
Hazem Mohamed <hazem.mohamed@alkifah.com>
2016-08-15 15:42:31 +0300

    Fix edit and view links in view stubs

--- Make/stubs/views/edit.blade.php
+++ Make/stubs/views/edit.blade.php
@@ -6,7 +6,7 @@
     <form action="{{'{{'}} route('{{$viewPrefix}}.destroy', ${{$single}})}}" class="pull-right" method="post">
         @{{csrf_field()}} @{{method_field('delete')}}
 
-        <a href="{{'{{'}} route('{{$viewPrefix}}.show')}}" class="btn btn-sm btn-info"><i class="fa fa-eye"></i> Show</a>
+        <a href="{{'{{'}} route('{{$viewPrefix}}.show', ${{$single}})}}" class="btn btn-sm btn-info"><i class="fa fa-eye"></i> Show</a>
         <button class="btn btn-sm btn-warning" type="submit"><i class="fa fa-trash-o"></i> Delete</button>
         <a href="{{'{{'}} route('{{$viewPrefix}}.index')}}" class="btn btn-sm btn-default"><i class="fa fa-chevron-left"></i> Back</a>
     </form>
--- Make/stubs/views/show.blade.php
+++ Make/stubs/views/show.blade.php
@@ -6,7 +6,7 @@
 <form action="{{'{{'}} route('{{$viewPrefix}}.destroy', ${{$single}})}}" class="pull-right" method="post">
     @{{csrf_field()}} @{{method_field('delete')}}
 
-    <a href="{{'{{'}} route('{{$viewPrefix}}.edit')}}" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i> Edit</a>
+    <a href="{{'{{'}} route('{{$viewPrefix}}.edit', ${{$single}})}}" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i> Edit</a>
     <button class="btn btn-sm btn-warning" type="submit"><i class="fa fa-trash-o"></i> Delete</button>
     <a href="{{'{{'}} route('{{$viewPrefix}}.index')}}" class="btn btn-sm btn-default"><i class="fa fa-chevron-left"></i> Back</a>
 </form>
