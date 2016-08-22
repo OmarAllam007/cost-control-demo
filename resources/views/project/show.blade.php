7adb693c862b869a982f27ea817a89ff98559041
Hazem Mohamed <hazem.mohamed@alkifah.com>
2016-08-22 13:00:05 +0300

    Add WBS Levels to show project page

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -1,23 +1,43 @@
 @extends('layouts.app')
 
 @section('header')
-<h2>Project</h2>
+    <h2 class="panel-title">Project - {{$project->name}}</h2>
 
-<form action="{{ route('project.destroy', $project)}}" class="pull-right" method="post">
-    {{csrf_field()}} {{method_field('delete')}}
+    <form action="{{ route('project.destroy', $project)}}" class="pull-right" method="post">
+        {{csrf_field()}} {{method_field('delete')}}
 
-    <a href="{{ route('project.edit', $project)}}" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i> Edit</a>
-    <button class="btn btn-sm btn-warning" type="submit"><i class="fa fa-trash-o"></i> Delete</button>
-    <a href="{{ route('project.index')}}" class="btn btn-sm btn-default"><i class="fa fa-chevron-left"></i> Back</a>
-</form>
+        <a href="{{ route('project.edit', $project)}}" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i>
+            Edit</a>
+        <button class="btn btn-sm btn-warning" type="submit"><i class="fa fa-trash-o"></i> Delete</button>
+        <a href="{{ route('project.index')}}" class="btn btn-sm btn-default"><i class="fa fa-chevron-left"></i> Back</a>
+    </form>
 @stop
 
 @section('body')
-{{ Form::model($project, ['route' => ['project.update', $project]]) }}
+    @if (trim($project->description))
+        <div class="panel panel-default">
+            <div class="panel-body">
+                {!! nl2br(e($project->description)) !!}
+            </div>
+        </div>
+    @endif
 
-{{ method_field('patch') }}
+    <section class="children">
+        <h3 class="page-header">WBS</h3>
 
-@include('project._form')
+        <div class="form-group clearfix">
+            <a href="{{route('wbs-level.create', ['project' => $project->id])}}" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i>
+                Add Level</a>
+        </div>
 
-{{ Form::close() }}
+        @if ($project->wbs_tree)
+            <ul class="list-unstyled tree">
+                @foreach($project->wbs_tree as $wbs_level)
+                    @include('wbs-level._recursive', compact('wbs_level'))
+                @endforeach
+            </ul>
+        @else
+            <div class="alert alert-info"><i class="fa fa-info"></i> No WBS found</div>
+        @endif
+    </section>
 @stop
