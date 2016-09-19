b7aa8ef2dfd00658313c7694e89ee9dfa8ad2493
Hazem Mohamed <hazem.mohamed@alkifah.com>
2016-09-19 09:28:02 +0300

    Show extra fields in resources list

--- resources/views/resources/index.blade.php
+++ resources/views/resources/index.blade.php
@@ -16,41 +16,45 @@
 
 @section('body')
     @if ($resources->total())
-        <table class="table table-condensed table-striped">
+        <table class="table table-condensed table-striped table-scroll-header">
             <thead>
             <tr>
 
-                <th>Resource Code</th>
-                <th>Name</th>
-                <th>Resource Type</th>
-                <th>Rate</th>
-                <th>Unit</th>
-                <th>Waste</th>
-                <th>Actions</th>
+                <th class="col-md-1">Resource Code</th>
+                <th class="col-md-3">Name</th>
+                <th class="col-md-3">Resource Type</th>
+                <th class="col-md-1">Rate</th>
+                <th class="col-md-1">Unit</th>
+                <th class="col-md-1">Waste</th>
+                <th class="col-md-2">Actions</th>
             </tr>
             </thead>
-            <tbody>
-            @foreach($resources as $resource)
-                <tr>
-                    <td class="col-md-1">{{ $resource->resource_code }}</td>
-                    <td class="col-md-3">{{ $resource->name }}</td>
-                    <td class="col-md-3">{{$resource->types->root->name or ''}}</td>
-                    <td class="col-md-1">{{ $resource->rate }}</td>
-                    <td class="col-md-1">{{ $resource->units->type or ''}}</td>
-                    <td class="col-md-1">{{ $resource->waste }}</td>
-
-                    <td class="col-md-2">
-                        <form action="{{ route('resources.destroy', $resource) }}" method="post">
-                            {{csrf_field()}} {{method_field('delete')}}
-                            <a class="btn btn-sm btn-primary" href="{{ route('resources.edit', $resource) }} "><i
-                                        class="fa fa-edit"></i> Edit</a>
-                            <button class="btn btn-sm btn-warning"><i class="fa fa-trash-o"></i> Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
         </table>
+        <div class="table-scroll">
+            <table class="table table-condensed table-striped">
+                <tbody>
+                @foreach($resources as $resource)
+                    <tr>
+                        <td class="col-md-1">{{ $resource->resource_code }}</td>
+                        <td class="col-md-3">{{ $resource->name }}</td>
+                        <td class="col-md-3">{{ $resource->types->root->name or '' }}</td>
+                        <td class="col-md-1">{{ $resource->rate }}</td>
+                        <td class="col-md-1">{{ $resource->units->type or ''}}</td>
+                        <td class="col-md-1">{{ $resource->waste }}</td>
+
+                        <td class="col-md-2">
+                            <form action="{{ route('resources.destroy', $resource) }}" method="post">
+                                {{csrf_field()}} {{method_field('delete')}}
+                                <a class="btn btn-sm btn-primary" href="{{ route('resources.edit', $resource) }} "><i
+                                            class="fa fa-edit"></i> Edit</a>
+                                <button class="btn btn-sm btn-warning"><i class="fa fa-trash-o"></i> Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
 
         {{ $resources->links() }}
     @else
