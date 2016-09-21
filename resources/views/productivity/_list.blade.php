a864f9b05f8e83293bb7f8e00cca32b8cef64a42
omar.garana <omar.garana@alkifah.com>
2016-09-21 10:18:29 +0300

    Update Productivity table color and columns of actions

--- resources/views/productivity/_list.blade.php
+++ resources/views/productivity/_list.blade.php
@@ -6,7 +6,7 @@
         <th class="col-xs-3">Crew Structure</th>
         <th class="col-xs-1">Unit</th>
         <th class="col-xs-2">Daily Output</th>
-        <th class="col-xs-1">Actions</th>
+        <th class="col-xs-2">Actions</th>
     </tr>
     </thead>
     <tbody>
@@ -18,7 +18,7 @@
             <td class="col-xs-3">{!! nl2br(e($productivity->crew_structure)) !!}</td>
             <td class="col-xs-1">{{ isset($productivity->units->type)?$productivity->units->type:'' }}</td>
             <td class="col-xs-2">{{number_format(floatval($productivity->daily_output), 2)}}</td>
-            <td class="col-xs-1">
+            <td class="col-xs-2">
                 <form action="{{ route('productivity.destroy', $productivity) }}" method="post">
                     {{csrf_field()}} {{method_field('delete')}}
                     <a class="btn btn-sm btn-primary" href="{{route('productivity.edit', $productivity)}}">
