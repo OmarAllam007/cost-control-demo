a9fe2073020360416d2cc203816db88eb767969b
Hazem Mohamed <hazem.mohamed@alkifah.com>
2016-09-28 10:40:06 +0300

    Make breakdown table scrollable

--- resources/views/project/tabs/_breakdown.blade.php
+++ resources/views/project/tabs/_breakdown.blade.php
@@ -69,56 +69,60 @@
 
 @if ($project->breakdown_resources->count())
     <div class="scrollpane">
-        <table class="table table-condensed table-bordered table-striped table-hover">
+        <table class="table table-condensed table-striped table-hover table-breakdown">
             <thead>
             <tr>
-                <th class="bg-black">WBS</th>
-                <th class="bg-primary">Activity</th>
-                <th class="bg-black">Breakdown Template</th>
-                <th class="bg-primary">Cost Account</th>
-                <th class="bg-success">Eng. Qty.</th>
-                <th class="bg-success">Budget Qty.</th>
-                <th class="bg-primary">Resource Qty.</th>
-                <th class="bg-success">Resource Waste</th>
-                <th class="bg-success">Resource Type</th>
-                <th class="bg-success">Resource Code</th>
-                <th class="bg-success">Resource Name</th>
-                <th class="bg-success">Price/Unit</th>
-                <th class="bg-success">Unit of measure</th>
-                <th class="bg-success">Budget Unit</th>
-                <th class="bg-success">Budget Cost</th>
-                <th class="bg-black">BOQ Equivalent Unit Rate</th>
-                <th class="bg-primary">No. Of Labors</th>
-                <th class="bg-success">Productivity (Unit/Day)</th>
-                <th class="bg-primary">Productivity Ref</th>
-                <th class="bg-success">Remarks</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-black">WBS</th>
+                <th style="min-width: 200px; max-width: 200px;" class="bg-primary">Activity</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-black">Breakdown Template</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-primary">Cost Account</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-success">Eng. Qty.</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-success">Budget Qty.</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-primary">Resource Qty.</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-success">Resource Waste</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-success">Resource Type</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-success">Resource Code</th>
+                <th style="min-width: 200px; max-width: 200px;" class="bg-success">Resource Name</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-success">Price/Unit</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-success">Unit of measure</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-success">Budget Unit</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-success">Budget Cost</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-black">BOQ Equivalent Unit Rate</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-primary">No. Of Labors</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-success">Productivity (Unit/Day)</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-primary">Productivity Ref</th>
+                <th style="min-width: 150px; max-width: 150px;" class="bg-success">Remarks</th>
             </tr>
             </thead>
+        </table>
+        <table class="table table-condensed table-striped table-hover table-breakdown">
             <tbody>
             @foreach($project->breakdown_resources as $resource)
                 <tr>
-                    <td class="bg-black">
+                    <td style="min-width: 150px; max-width: 150px;" class="bg-black">
                         <abbr title="{{$resource->breakdown->wbs_level->path}}">{{$resource->breakdown->wbs_level->code}}</abbr>
                     </td>
-                    <td class="bg-primary">{{$resource->breakdown->std_activity->name}}</td>
-                    <td class="bg-black">{{$resource->breakdown->template->name}}</td>
-                    <td class="bg-primary">{{$resource->breakdown->cost_account}}</td>
-                    <td class="">{{number_format($resource->eng_qty, 2)}}</td>
-                    <td class="">{{number_format($resource->budget_qty, 2)}}</td>
-                    <td class="bg-primary">{{number_format($resource->resource_qty, 2)}}</td>
-                    <td class="">{{number_format($resource->project_resource->resource_waste, 2)}}%</td>
-                    <td class="">{{$resource->resource->types->root->name or ''}}</td>
-                    <td class="">{{$resource->resource->resource->resource_code or ''}}</td>
-                    <td class="">{{$resource->resource->resource->name or ''}}</td>
-                    <td class="">{{$resource->project_resource->rate or ''}}</td>
-                    <td class="">{{$resource->project_resource->units->type or ''}}</td>
-                    <td class="">{{number_format($resource->budget_unit, 2)}}</td>
-                    <td class="">{{number_format($resource->budget_cost, 2)}}</td>
-                    <td class="bg-black">{{number_format($resource->boq_unit_rate, 2)}}</td>
-                    <td class="bg-primary">{{$resource->labor_count or ''}}</td>
-                    <td class="">{{$resource->project_productivity->after_reduction or ''}}</td>
-                    <td class="bg-primary">{{$resource->productivity->csi_code or ''}}</td>
-                    <td class="">{{$resource->remarks}}</td>
+                    <td style="min-width: 200px; max-width: 200px;" class="bg-primary">{{$resource->breakdown->std_activity->name}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="bg-black">{{$resource->breakdown->template->name}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="bg-primary">{{$resource->breakdown->cost_account}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="">{{number_format($resource->eng_qty, 2)}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="">{{number_format($resource->budget_qty, 2)}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="bg-primary">{{number_format($resource->resource_qty, 2)}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="">{{number_format($resource->project_resource->resource_waste, 2)}}
+                        %
+                    </td>
+                    <td style="min-width: 150px; max-width: 150px;" class="">{{$resource->resource->types->root->name or ''}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="">{{$resource->resource->resource->resource_code or ''}}</td>
+                    <td style="min-width: 200px; max-width: 200px;" class="">{{$resource->resource->resource->name or ''}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="">{{$resource->project_resource->rate or ''}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="">{{$resource->project_resource->units->type or ''}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="">{{number_format($resource->budget_unit, 2)}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="">{{number_format($resource->budget_cost, 2)}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="bg-black">{{number_format($resource->boq_unit_rate, 2)}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="bg-primary">{{$resource->labor_count or ''}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="">{{$resource->project_productivity->after_reduction or ''}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="bg-primary">{{$resource->productivity->csi_code or ''}}</td>
+                    <td style="min-width: 150px; max-width: 150px;" class="">{{$resource->remarks}}</td>
                 </tr>
             @endforeach
             </tbody>
