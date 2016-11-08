bb8ee8dddb08882501b09da71935b4bea4e740df
Hazem Mohamed <hazem.mohamed@alkifah.com>
2016-11-08 09:24:31 +0300

    Start working on BOQ

--- resources/assets/js/project/components/Boq.js
+++ resources/assets/js/project/components/Boq.js
@@ -1,8 +1,11 @@
 export default {
-    template: document.getElementById('#BOQTemplate'),
+    template: document.getElementById('#BOQTemplate').innerHTML,
 
     data() {
-
+        return {
+            items: {},
+            loading: false
+        };
     },
 
     methods: {
@@ -14,6 +17,18 @@ export default {
     },
 
     events: {
-
+        wbs_changed(params) {
+            this.loading = true;
+            $.ajax({
+                url: '/api/wbs/boq/' + params.selection, dataType: 'json',
+                cache: true
+            }).success(response => {
+                this.loading = false;
+                this.boq = response;
+            }).error(() => {
+                this.loading = false;
+                this.boq = {};
+            });
+        }
     }
 }
