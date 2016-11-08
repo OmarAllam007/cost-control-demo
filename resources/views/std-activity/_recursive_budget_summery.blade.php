949b89fc3d4abf773bd628fad6ea500d2a1d50a4
Omar Khaled <omar.coder007@gmail.com>
2016-11-08 10:56:41 +0300

    Budget Summery

--- resources/views/std-activity/_recursive_budget_summery.blade.php
+++ resources/views/std-activity/_recursive_budget_summery.blade.php
@@ -1,40 +1,45 @@
 <li>
     <div class="tree--item">
-        <div class="tree--item--label
-@if($tree_level ==0)
-                blue-second-level
-        @elseif($tree_level ==1)
-                blue-third-level
-           @elseif($tree_level ==2)
-                blue-fourth-level
-           @else
-                blue-fourth-level
-           @endif
-                ">
+        <div class="tree--item--label blue-first-level">
             {{$division['name']}}
             <strong class="pull-right">{{number_format($division['budget_cost'],2)}}</strong>
         </div>
     </div>
+    <ul class="list-unstyled tree">
+        <li>
+            @if ($division['divisions'])
+                @foreach($division['divisions'] as $div)
+                    <div class="tree--item">
+                        <div class="tree--item--label blue-second-level">
+                            {{$div['division_name']}}
+                            <strong class="pull-right">{{number_format($div['budget_cost'],2)}}</strong>
+                            </div>
+                        </div>
+                            <article id="children-{{$division['id']}}" class="tree--child">
+                                @if ($div['activities'])
+                                    <table class="table  table-condensed">
+                                        <thead>
+                                        <tr class="activity-header">
+                                            <th>Activity</th>
+                                            <th width="200px"><span class="pull-right">Budget Cost</span></th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
 
-    <article id="children-{{$division['id']}}" class="tree--child">
-
-        @if ($division['activities'])
-            <table class="table  table-condensed">
-                <thead>
-                <tr class="activity-header">
-                    <th>Activity</th>
-                    <th width="200px"><span class="pull-right">Budget Cost</span></th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($division['activities'] as $activity)
-                    <tr>
-                        <td>{{$activity['name']}}</td>
-                        <td><span class="pull-right">{{number_format($activity['budget_cost'],2)}}</span></td>
-                    </tr>
+                                        @foreach($div['activities'] as $activity)
+                                            <tr>
+                                                <td>{{$activity['name']}}</td>
+                                                <td>
+                                                    <span class="pull-right">{{number_format($activity['budget_cost'],2)}}</span>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                @endif
+                            </article>
                 @endforeach
-                </tbody>
-            </table>
-        @endif
-    </article>
+            @endif
+        </li>
+    </ul>
 </li>
