3d8178821e7f1de7c977e11b5453beb4d9f28947
Hazem Mohamed <hazem.mohamed@alkifah.com>
2016-11-20 14:02:27 +0300

    Separate cost control from budget in show project

--- app/Http/Routes/hazem.php
+++ app/Http/Routes/hazem.php
@@ -90,6 +90,11 @@ Route::group(['prefix' => 'breakdown'], function(){
     Route::delete('wipe/{project}', ['as' => 'breakdown.wipe', 'uses' => 'BreakdownResourceController@wipe']);
 });
 
+Route::group(['prefix' => 'project', 'as' => 'project.'], function () {
+    Route::get('budget/{project}', ['as' => 'budget', 'uses' => 'ProjectController@show']);
+    Route::get('cost-control/{project}', ['as' => 'cost-control', 'uses' => 'ProjectController@costControl']);
+});
+
 Route::resource('project', 'ProjectController');
 Route::resource('wbs-level', 'WbsLevelController');
 Route::resource('std-activity', 'StdActivityController');
