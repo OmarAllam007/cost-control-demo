9f0cac296c8c3db531a9d0933764f8dbea6bf5df
omar <omar.coder007@gmail.com>
2016-11-22 16:00:50 +0300

    Seeder and get resources

--- app/Http/Controllers/Api/WbsController.php
+++ app/Http/Controllers/Api/WbsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Boq;
 use App\BreakdownResource;
+use App\BreakDownResourceShadow;
 use App\Formatters\BreakdownResourceFormatter;
 use App\Jobs\CacheWBSTree;
 use App\Project;
@@ -27,24 +28,32 @@ class WbsController extends Controller
 
     function breakdowns(WbsLevel $wbs_level)
     {
-        $resources = BreakdownResource::with('breakdown')
-//            ->with('std_activity_resource')
-            ->with('breakdown.template')
-            ->with('breakdown.project')
-//            ->with('breakdown.variables')
-            ->with('template_resource')
-            ->with('template_resource.resource')
-            ->with('template_resource')
-//            ->with('productivity')
-            ->forWbs($wbs_level->id)->get()
-            ->map(function (BreakdownResource $res) {
-                return new BreakdownResourceFormatter($res);
-            });
+
+
+        $resources = BreakDownResourceShadow::where('wbs_id',$wbs_level->id)->get()->map(function (BreakDownResourceShadow $res) {
+            return $res;
+        });
+
+//        $resources = BreakdownResource::with('breakdown')
+////            ->with('std_activity_resource')
+//            ->with('breakdown.template')
+//            ->with('breakdown.project')
+////            ->with('breakdown.variables')
+//            ->with('template_resource')
+//            ->with('template_resource.resource')
+//            ->with('template_resource')
+////            ->with('productivity')
+//            ->forWbs($wbs_level->id)->get()
+//            ->map(function (BreakdownResource $res) {
+//                return new BreakdownResourceFormatter($res);
+//            });
 //        $r = new Response();
 //        $r->header('Content-Type', 'text/html');
 //        $r->setContent(json_encode($resources));
 //        return $r;
+
         return $resources;
+
     }
 
     function boq(WbsLevel $wbs_level)
