50412e359d183b82c394ac37bde892aee3533f81
omar <omar.coder007@gmail.com>
2016-12-10 13:24:38 +0300

    Order CSI Cateogry

--- app/Http/ViewComposers/CsiCategoryComposer.php
+++ app/Http/ViewComposers/CsiCategoryComposer.php
@@ -19,6 +19,7 @@ class CsiCategoryComposer
         $categoryTree = \Cache::remember('csi-tree', 7 * 24 * 60, function () {
             return dispatch(new CacheCsiCategoryTree());
         });
+        ksort($categoryTree);
         $view->with(compact('categoryTree'));
     }
 
