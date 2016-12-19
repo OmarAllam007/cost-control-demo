25a618a43f1586f964a45115ce58e36ed0b96351
omar <omar.coder007@gmail.com>
2016-12-19 11:16:13 +0300

    Templates Authentication

--- resources/views/project/tabs/_breakdown_template.blade.php
+++ resources/views/project/tabs/_breakdown_template.blade.php
@@ -1,5 +1,6 @@
 <section id="BreakdownTemplateArea" class="project-tab">
     <div class="form-group tab-actions clearfix">
+        @can('breakdown_templates',$project)
         <div class="pull-right">
             <a href="{{route('breakdown-template.create', ['project' => $project])}}"
                class="btn btn-primary btn-sm in-iframe" title="Add Template">
@@ -14,13 +15,17 @@
 
 
         </div>
+            @endcan
     </div>
     @if ($project->templates->count())
         <table class="table table-condensed table-striped table-fixed">
             <thead>
             <tr>
                 <th class="col-xs-8">Name</th>
-                <th class="col-xs-4">Actions</th>
+
+                <th class="col-xs-4"> @can('breakdown_templates',$project)
+                    Actions @endcan </th>
+
             </tr>
             </thead>
             <tbody>
@@ -29,11 +34,14 @@
                     <td class="col-xs-8"><a
                                 href="/breakdown-template/{{$breakdown_template->id}}?project_id={{$project->id}}">{{ $breakdown_template->name }}</a>
                     </td>
+
                     <td class="col-xs-4">
-                        <form action="{{ route('breakdown-template.destroy', $breakdown_template->id) }}" method="post">
-                            {{csrf_field()}} {{method_field('delete')}}
-                            <button class="btn btn-sm btn-warning"><i class="fa fa-trash-o"></i> Delete</button>
-                        </form>
+                        @can('breakdown_templates',$project)
+                            <form action="{{ route('breakdown-template.destroy', $breakdown_template->id) }}" method="post">
+                                {{csrf_field()}} {{method_field('delete')}}
+                                <button class="btn btn-sm btn-warning"><i class="fa fa-trash-o"></i> Delete</button>
+                            </form>
+                        @endcan
 
                     </td>
                 </tr>
