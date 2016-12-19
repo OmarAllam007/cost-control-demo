57351d43b9e1a1258b4aa5cef27472f0fecd6c6e
omar <omar.coder007@gmail.com>
2016-12-19 09:40:06 +0300

    Resources filter

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -20,7 +20,7 @@
     <nav class="project-nav">
         @can('budget', $project)
             <a href="#wbsArea" class="btn btn-primary">WBS &amp; Activity</a>
-            <a href="#ResourcesArea" class="btn btn-outline btn-primary">Resources</a>
+            <a href="#ProjectResources" class="btn btn-outline btn-primary">Resources</a>
             <a href="#ProductivityArea" class="btn btn-outline btn-primary">Productivity</a>
             <a href="#BreakdownTemplateArea" class="btn btn-outline btn-primary">Breakdown Templates</a>
         @endcan
