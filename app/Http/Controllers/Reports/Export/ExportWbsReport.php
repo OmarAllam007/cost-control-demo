1f93c20bc44a6ae5ded1353e99fd0bfe011941cb
omar <omar.coder007@gmail.com>
2017-01-05 12:13:44 +0300

    Export WBS-Report

--- app/Http/Controllers/Reports/Export/ExportWbsReport.php
+++ app/Http/Controllers/Reports/Export/ExportWbsReport.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Reports\Export;
 use App\Http\Requests\Request;
 use App\Jobs\CacheWBSTree;
 use App\Project;
+use phpDocumentor\Reflection\Types\Null_;
 
 class ExportWbsReport
 {
@@ -15,63 +16,94 @@ class ExportWbsReport
         /** @var $objPHPExcel ->getActiveSheet(); $sheet */
 
         $sheet = $objPHPExcel->getActiveSheet();
-        $sheet->fromArray(['WBS_LEVEL-1', 'WBS_LEVEL-2', 'WBS_LEVEL-3', 'WBS_LEVEL-4'], 'A1');
+        $sheet->fromArray(['WBS_LEVEL-1', 'WBS_LEVEL-2', 'WBS_LEVEL-3', 'WBS_LEVEL-4', 'WBS_LEVEL-5', 'WBS_LEVEL-6', 'WBS_LEVEL-7'], 'A1');
         $rowCount = 2;
         $col = 0;
-        $varnumber = 5;
-        $colstart = 4;
         $data = dispatch(new CacheWBSTree($project));
-        $item = $this->getDepth($data);
         foreach ($data as $level) {
+            $rowCount++;
             $sheet->setCellValueByColumnAndRow($col, $rowCount, $level['name']);
             if (count($level['children'])) {
                 $col++;
                 foreach ($level['children'] as $fChild) {
+                    $rowCount++;
                     $sheet->setCellValueByColumnAndRow($col, $rowCount, $fChild['name']);
                     if (count($fChild['children'])) {
-                        $rowCount++;
                         $col++;
                         foreach ($fChild['children'] as $sChild) {
+                            $rowCount++;
                             $sheet->setCellValueByColumnAndRow($col, $rowCount, $sChild['name']);
-                            $col++;
                             if (count($sChild['children'])) {
-                                $rowCount++;
+                                $col++;
                                 foreach ($sChild['children'] as $thChild) {
-                                    $sheet->setCellValueByColumnAndRow($colstart, 1, 'WBS_LEVEL-' . $varnumber);
+                                    $rowCount++;
                                     $sheet->setCellValueByColumnAndRow($col, $rowCount, $thChild['name']);
-                                    $colstart++;
-                                    $varnumber++;
-                                    $col++;
+                                    if (count($thChild['children'])) {
+                                        $col++;
+                                        foreach ($thChild['children'] as $fourthChild) {
+                                            $rowCount++;
+                                            $sheet->setCellValueByColumnAndRow($col, $rowCount, $fourthChild['name']);
+
+                                            if (count($fourthChild['children'])) {
+                                                $col++;
+                                                foreach ($fourthChild['children'] as $fifthChild) {
+                                                    $rowCount++;
+                                                    $sheet->setCellValueByColumnAndRow($col, $rowCount, $fifthChild['name']);
+
+                                                    if (count($fifthChild['children'])) {
+                                                        $col++;
+                                                        foreach ($fifthChild['children'] as $sixthChild) {
+                                                            $rowCount++;
+                                                            $sheet->setCellValueByColumnAndRow($col, $rowCount, $sixthChild['name']);
+                                                            if (count($sixthChild['children'])) {
+                                                                $col++;
+                                                                foreach ($sixthChild['children'] as $seventhChild) {
+                                                                    $rowCount++;
+                                                                    $sheet->setCellValueByColumnAndRow($col, $rowCount, $seventhChild['name']);
+                                                                }
+                                                                $col--;
+                                                            }
+                                                        }
+                                                        $col--;
+                                                    }
+                                                }
+                                                $col--;
+                                            }
+                                        }
+                                        $col--;
+                                    }
                                 }
-                                $rowCount--;
+                                $col--;
                             }
                         }
                         $col = 1;
                     }
-                    $rowCount++;
                 }
-                $rowCount++;
+//                $rowCount++;
             }
 
-
+            $col = 0;
         }
+
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment;filename="' . $project->name . ' - WBSReport.xlsx"');
         header('Cache-Control: max-age=0');
         $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
         $objWriter->save('php://output');
     }
-    function getDepth($a) {
-        $max=0;
+
+    function getDepth($a)
+    {
+        $max = 0;
         foreach ($a as $val) {
             if (is_array($val)) {
-                $tmp_depth=$this->getDepth($val);
-                if ($max<($tmp_depth)) {
-                    $max=$tmp_depth;
+                $tmp_depth = $this->getDepth($val);
+                if ($max < ($tmp_depth)) {
+                    $max = $tmp_depth;
                 }
             }
         }
-        return $max+1;
+        return $max + 1;
     }
 
 
