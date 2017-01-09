16c288f28621df04cbaaea7572387e7963d1012a
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-01-09 11:36:11 +0300

    Fix resources without type

--- app/Resources.php
+++ app/Resources.php
@@ -68,8 +68,8 @@ class Resources extends Model
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'type' => $this->types->name,
-            'unit' => isset($this->units->type) ? $this->units->type : '',
+            'type' => $this->types->name ?? '',
+            'unit' => $this->units->type ?? '',
             'rate' => $this->rate,
             'root_type' => $this->types->root->name,
             'resource_type_id' => $this->types->root->id,
