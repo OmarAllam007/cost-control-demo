6d8de5f4614c9486aeb3824303fa31970f6a74ba
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-01-10 10:13:18 +0300

    Add pagination to budget resources to enhance performance

--- resources/assets/js/project/components/Resources.js
+++ resources/assets/js/project/components/Resources.js
@@ -1,3 +1,5 @@
+import Pagination from './pagination';
+
 export default{
     // template: document.getElementById(''),
     props: ['project'],
@@ -6,38 +8,57 @@ export default{
             resources: [],
             resource: '',
             resource_type: '',
+            count: 0,
+            first: 1,
+            last: 100
         }
     },
+
     methods: {
         loadResources(){
             $.ajax({
                 url: '/api/resources/resources/' + this.project, dataType: 'json',
-            }).success(response=> {
+            }).success(response => {
                 this.resources = response;
-            }).error(response=> {
+            }).error(response => {
                 console.log('error');
             });
         }
     },
+
     computed: {
         filtered_resources(){
-            return this.resources.filter((item)=> {
+            const resources = this.resources.filter((item) => {
                 if (this.resource) {
                     return item.name.toLowerCase().indexOf(this.resource.toLowerCase()) >= 0
                         || item.resource_code.toLowerCase().indexOf(this.resource.toLowerCase()) >= 0;
                 }
                 return true;
-            }).filter((item)=> {
+            }).filter((item) => {
                 if (this.resource_type) {
                     return (item.resource_type_id == this.resource_type);
                 }
                 return true;
             });
+
+            this.count = resources.length;
+            return resources.slice(this.first, this.last);
         }
     },
-    watch: {},
-    ready(){
+
+    created(){
         this.loadResources();
+    },
+
+    events: {
+        pageChanged(params) {
+            this.first = params.first;
+            this.last = params.last;
+        }
+    },
+
+    components: {
+        Pagination
     }
 
 }
