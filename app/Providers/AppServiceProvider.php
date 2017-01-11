6f70ec6dce252573834657ec6e75ec57400410fa
omar <omar.coder007@gmail.com>
2017-01-11 16:02:03 +0300

    QS Report edit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -63,7 +63,6 @@ class AppServiceProvider extends ServiceProvider
         Survey::observe(QuantitySurveyObserver::class);
         StdActivityResource::observe(StandardActivityResourceObserver::class);
         BreakdownVariable::observe(BreakdownVariableObserver::class);
-
     }
 
 
