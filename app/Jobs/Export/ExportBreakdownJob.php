7454dd6a13094679efaf690f2a71384788cf0e1d
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-01-29 09:34:27 +0300

    Fix breakdown export memory error

--- app/Jobs/Export/ExportBreakdownJob.php
+++ app/Jobs/Export/ExportBreakdownJob.php
@@ -3,6 +3,7 @@
 namespace App\Jobs\Export;
 
 use App\Jobs\Job;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class ExportBreakdownJob extends Job
 {
@@ -19,6 +20,7 @@ class ExportBreakdownJob extends Job
     public function handle()
     {
         set_time_limit(600);
+        $filename = storage_path('app/'. uniqid('breakdown_csv_'));
 
         $headers = [
             'WBS-Level-1', 'WBS-Level-2', 'WBS-Level-3', 'WBS-Level-4','WBS-Level-5','WBS-Level-6','WBS-Level-7','Activity-Division-1','Activity-Division-2','Activity-Division-3',
@@ -29,82 +31,88 @@ class ExportBreakdownJob extends Job
             'BOQ Equivalent Unit Rate','No. Of Labors', 'Productivity (Unit/Day)', 'Productivity Reference', 'Remarks'
         ];
 
-        $lines = [
-            implode(",", array_map([$this, 'csv_quote'], $headers))
-        ];
+        $line = implode(",", array_map([$this, 'csv_quote'], $headers));
+        $fh = fopen($filename, 'w');
+        fwrite($fh, $line);
 
-        $shadows = $this->project->shadows->load('std_activity', 'std_activity.division.parent.parent.parent', 'wbs', 'wbs.parent.parent.parent');
-        foreach ($shadows as $breakdown_resource) {
-            $discpline = $breakdown_resource->std_activity->discipline;
-            $division = $breakdown_resource->std_activity->division;
-            $level = $breakdown_resource->wbs;
-            $levels = [];
-            $divisions=[];
-            $levels[] = $level->name;
+        /** @var LengthAwarePaginator $shadows */
+        $shadows = $this->project->shadows()->with('std_activity', 'std_activity.division.parent.parent.parent', 'wbs', 'wbs.parent.parent.parent')->chunk(20000, function($shadows) use ($fh) {
+            foreach ($shadows as $breakdown_resource) {
+                $discpline = $breakdown_resource->std_activity->discipline;
+                $division = $breakdown_resource->std_activity->division;
+                $level = $breakdown_resource->wbs;
+                $levels = [];
+                $divisions=[];
+                $levels[] = $level->name;
 
-            $parent = $level->parent;
-            while ($parent) {
-                $levels[] = $parent->name;
-                $parent = $parent->parent;
-            };
-            $levels = array_reverse($levels);
+                $parent = $level->parent;
+                while ($parent) {
+                    $levels[] = $parent->name;
+                    $parent = $parent->parent;
+                };
+                $levels = array_reverse($levels);
 
-            $divisions[]=$division->name;
+                $divisions[]=$division->name;
 
-            $parentDiv = $division->parent;
-            while($parentDiv){
-                $divisions[] = $parentDiv->name;
-                $parentDiv= $parentDiv->parent;
+                $parentDiv = $division->parent;
+                while($parentDiv){
+                    $divisions[] = $parentDiv->name;
+                    $parentDiv= $parentDiv->parent;
+                }
+                $divisions = array_reverse($divisions);
+                $data = [
+                    isset($levels[0]) ? $levels[0] : '',
+                    isset($levels[1]) ? $levels[1] : '',
+                    isset($levels[2]) ? $levels[2] : '',
+                    isset($levels[3]) ? $levels[3] : '',
+                    isset($levels[4]) ? $levels[4] : '',
+                    isset($levels[5]) ? $levels[5] : '',
+                    isset($levels[6]) ? $levels[6] : '',
+                    isset($divisions[0]) ? $divisions[0] : '',
+                    isset($divisions[1]) ? $divisions[1] : '',
+                    isset($divisions[2]) ? $divisions[2] : '',
+                    isset($divisions[3]) ? $divisions[3] : '',
+                    $breakdown_resource['code'],
+                    $breakdown_resource['activity'],
+                    $discpline,
+                    $breakdown_resource['template'],
+                    $breakdown_resource['cost_account'],
+                    $breakdown_resource['eng_qty'],
+                    $breakdown_resource['budget_qty'],
+                    $breakdown_resource['resource_qty'],
+                    $breakdown_resource['resource_waste'],
+                    $breakdown_resource['resource_type'],
+                    $breakdown_resource['resource_code'],
+                    $breakdown_resource['resource_name'],
+                    $breakdown_resource['unit_price'],
+                    $breakdown_resource['measure_unit'],
+                    $breakdown_resource['budget_unit'],
+                    $breakdown_resource['budget_cost'],
+                    $breakdown_resource['boq_equivilant_rate'],
+                    $breakdown_resource['labors_count'],
+                    $breakdown_resource['productivity_output'],
+                    $breakdown_resource['productivity_ref'],
+                    $breakdown_resource['remarks'],
+                ];
+
+                $line = PHP_EOL . implode(",", array_map([$this, 'csv_quote'], $data));
+                fwrite($fh, $line);
+
+                unset($levels, $level, $division, $divisions, $discpline, $line);
             }
-            $divisions = array_reverse($divisions);
-            $data = [
-                isset($levels[0]) ? $levels[0] : '',
-                isset($levels[1]) ? $levels[1] : '',
-                isset($levels[2]) ? $levels[2] : '',
-                isset($levels[3]) ? $levels[3] : '',
-                isset($levels[4]) ? $levels[4] : '',
-                isset($levels[5]) ? $levels[5] : '',
-                isset($levels[6]) ? $levels[6] : '',
-                isset($divisions[0]) ? $divisions[0] : '',
-                isset($divisions[1]) ? $divisions[1] : '',
-                isset($divisions[2]) ? $divisions[2] : '',
-                isset($divisions[3]) ? $divisions[3] : '',
-                $breakdown_resource['code'],
-                $breakdown_resource['activity'],
-                $discpline,
-                $breakdown_resource['template'],
-                $breakdown_resource['cost_account'],
-                $breakdown_resource['eng_qty'],
-                $breakdown_resource['budget_qty'],
-                $breakdown_resource['resource_qty'],
-                $breakdown_resource['resource_waste'],
-                $breakdown_resource['resource_type'],
-                $breakdown_resource['resource_code'],
-                $breakdown_resource['resource_name'],
-                $breakdown_resource['unit_price'],
-                $breakdown_resource['measure_unit'],
-                $breakdown_resource['budget_unit'],
-                $breakdown_resource['budget_cost'],
-                $breakdown_resource['boq_equivilant_rate'],
-                $breakdown_resource['labors_count'],
-                $breakdown_resource['productivity_output'],
-                $breakdown_resource['productivity_ref'],
-                $breakdown_resource['remarks'],
-            ];
+        });
 
-            $lines[] = implode(",", array_map([$this, 'csv_quote'], $data));
-
-            unset($levels, $level, $division, $divisions, $discpline);
-        }
-        header('Content-Type: text/csv');
-        header('Content-Disposition: attachment;filename="' . $this->project->name . ' - BreakDown.csv"');
+//
+//        header('Content-Type: text/csv');
+//        header('Content-Disposition: attachment;filename="' . $this->project->name . ' - BreakDown.csv"');
         unset($shadows);
 //        header('Cache-Control: max-age=0');
 //        $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
 //        $objWriter->save('php://output');
 
-        file_put_contents('php://output', implode(PHP_EOL, $lines));
+fclose($fh);
 
+        return $filename;
     }
 
     protected function csv_quote($str)
