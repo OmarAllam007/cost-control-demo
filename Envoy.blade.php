c0c47235dbd1d07d62a342f6b257142eda259827
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-01-30 07:21:24 +0000

    Add envoy file

---
+++ Envoy.blade.php
@@ -0,0 +1,22 @@
+@servers(['web' => 'app@budget.alkifahcont.com'])
+
+@task('deploy')
+    cd cost-control
+    git pull origin master
+@endtask
+
+@task('migrate')
+    cd cost-control
+    php artisan optimize
+    php artisan migrate
+@endtask
+
+@task('cache:clear')
+    cd cost-control
+    php artisan cache:clear
+@endtask
+
+@task('composer')
+    cd cost-control
+    composer install
+@endtask
