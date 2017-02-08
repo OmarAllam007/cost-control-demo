9f0257890d64e1a598ad63c4927a1b55b276a168
omar <omar.coder007@gmail.com>
2017-02-08 11:27:07 +0300

    Budget Dry Discpline

--- app/WbsLevel.php
+++ app/WbsLevel.php
@@ -169,8 +169,8 @@ class WbsLevel extends Model
 
     }
 
-    function getDry(){
-        $boq = Boq::where('wbs_id',$this->id)->first();
+    function getDry($project){
+        $boq = Boq::where('wbs_id',$this->id)->where('project_id',$project->id)->first();
         if(isset($boq->dry_ur)){//chenged from $boq->dry_ur to 1
             return 1;
         }
