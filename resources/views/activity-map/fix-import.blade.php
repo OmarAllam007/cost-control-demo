57861930b1463e8ceb3ae310a01dd974548cab2d
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-02-10 10:07:47 +0300

    Show invalid activity mapping entries to fix

--- resources/views/activity-map/fix-import.blade.php
+++ resources/views/activity-map/fix-import.blade.php
@@ -49,8 +49,8 @@
         </div>
     </div>
 
-    <div class="modal fade" tabindex="-1" role="dialog" id="ActivityModal">
-        <div class="modal-dialog">
+    <div class="modal fade" tabindex="-1" role="dialog" id="SelectActivityModal">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
@@ -58,29 +58,78 @@
                 </div>
                 <div class="modal-body">
                     <div class="form-group">
-                        <input type="search" id="SearchActivity">
+                        <input type="search" id="SearchActivity" class="form-control"
+                               placeholder="Please type to search">
                     </div>
 
-                    <table class="table table-striped table-condensed table-hover">
+                    <table class="table table-striped table-condensed table-hover table-fixed">
                         <thead>
                         <tr>
-                            <th>Code</th>
-                            <th>WBS</th>
-                            <th>Activity</th>
+                            <th class="col-sm-3">Code</th>
+                            <th class="col-sm-6">WBS</th>
+                            <th class="col-sm-3">Activity</th>
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            @foreach($codes as $code => $activity)
-                                <td><a heref="#" class="select-activity">{{$code}}</a></td>
-                                <td>{{$activity->wbs->path}}</td>
-                                <td>{{$activity->activity}}</td>
-                            @endforeach
-                        </tr>
+                        @foreach($codes as $activity)
+                            <tr>
+                                <td class="col-sm-3"><a href="#" class="select-code">{{$activity->code}}</a></td>
+                                <td class="col-sm-6">{{$activity->wbs->path}}</td>
+                                <td class="col-sm-3">{{$activity->activity}}</td>
+                            </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('javascript')
+    <script>
+        $(function () {
+            var modal = $('#SelectActivityModal').on('shown.bs.modal', function () {
+                $(this).find('input').focus();
+            });
+
+            var table = modal.find('table');
+
+            $('.select-activity').on('click', function (e) {
+                e.preventDefault();
+
+                modal.data('target', this);
+                modal.modal();
+            });
+
+            $('#SearchActivity').on('keyup', function () {
+                var val = $.trim(this.value.toLowerCase());
+                if (val) {
+                    table.find('tbody tr').each(function () {
+                        var tr = $(this);
+                        var show = false;
+                        tr.find('td').each(function() {
+                            if ($(this).text().toLowerCase().indexOf(val) != -1) {
+                                show = true;
+                                return true;
+                            }
+                        });
+
+                        show? tr.show() : tr.hide();
+                    });
+                } else {
+                    table.find('tbody tr').show();
+                }
+            });
+
+            modal.find('.select-code').on('click', function(e) {
+                e.preventDefault();
+                var code = $(this).text();
+                var target = $(modal.data('target'));
+                target.siblings('input').val(code);
+                target.text(code);
+                modal.modal('hide');
+            });
+        });
+    </script>
 @endsection
