a5a93e42196797d64649cc2dcfd2f181912eb66d
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-02-12 15:38:25 +0300

    Add invalid issue in issues report

--- resources/views/actual-batches/issues/account_distribution.blade.php
+++ resources/views/actual-batches/issues/account_distribution.blade.php
@@ -1,7 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: hazem
- * Date: 2/8/17
- * Time: 3:15 PM
- */
+@php
+    $data = json_decode($issue->data, true);
+@endphp
+
+<article class="panel panel-default">
+    <div class="panel-heading">
+        <h4 class="panel-title">Multiple Cost Account</h4>
+    </div>
+
+    <table class="table table-condensed table-bordered">
+        <thead>
+        <tr>
+
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($data as $resource)
+            <tr>
+
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</article>
+
--- resources/views/actual-batches/issues/activity_mapping_unprivileged.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: hazem
- * Date: 2/7/17
- * Time: 4:33 PM
- */
