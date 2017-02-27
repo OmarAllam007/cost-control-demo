7128db642296857cb839cdfdf6fd134186cd3f00
omar <omar.coder007@gmail.com>
2017-02-27 08:31:43 +0300

    Resource Dictionary

--- app/Http/Controllers/Reports/CostReports/ResourceCodeReport.php
+++ app/Http/Controllers/Reports/CostReports/ResourceCodeReport.php
@@ -42,29 +42,27 @@ GROUP BY resource_id , resource_name', [$project->id]))->map(function ($resource
             $this->resources->put($resource->resource_id, ['unit' => $resource->unit_price, 'budget_unit' => $resource->budget_unit, 'budget_cost' => $resource->budget_cost]);
         });
 
-        collect(\DB::select('SELECT
+        collect(\DB::select('
+SELECT
   c.resource_id,
-  c.to_date_unit_price             AS to_date_unit_price,
+  sum(c.to_date_unit_price) /COUNT(c.resource_id)                 AS to_date_unit_price,
   SUM(c.to_date_qty)                     AS to_date_qty,
-  SUM(c.to_date_cost)                    AS to_data_cost,
+  SUM(c.to_date_cost)                    AS to_date_cost,
   SUM(c.allowable_ev_cost)               AS to_date_allowable_cost,
   SUM(c.qty_var)               AS quantity_var,
   SUM(c.allowable_var)                   AS allowable_var,
-  c.remaining_unit_price            AS remaining_unit_price,
+  sum(remaining_unit_price) /COUNT(c.resource_id)             AS remaining_unit_price,
   SUM(c.remaining_qty)                   AS remaining_qty,
   SUM(c.remaining_cost)                  AS remain_cost,
-  SUM(c.completion_unit_price)           AS completion_unit_price,
+  sum(completion_unit_price) /COUNT(c.resource_id)           AS completion_unit_price,
   SUM(c.completion_qty)                  AS completion_qty,
   SUM(c.completion_cost)                 AS completion_cost,
   SUM(c.cost_var)                        AS cost_var,
   SUM(c.pw_index) / COUNT(c.resource_id) AS pw_index
 FROM cost_shadows c
-WHERE c.project_id = ? AND c.period_id = ?
-GROUP BY c.resource_id ,c.to_date_unit_price ,c.remaining_unit_price ,c.completion_unit_price', [$project->id, $chosen_period_id]))->map(function ($resource) {
+WHERE c.project_id = ? AND c.period_id = ? 
+GROUP BY c.resource_id', [$project->id, $chosen_period_id]))->map(function ($resource) {
             $this->period_cost->put($resource->resource_id, [
-                'unit_price' => $resource->unit_price ?? 0,
-                'budget_unit' => $resource->budget_unit ?? 0,
-                'budget_cost' => $resource->budget_cost ?? 0,
                 'to_date_unit_price' => $resource->to_date_unit_price ?? 0,
                 'to_date_qty' => $resource->to_date_qty ?? 0,
                 'to_date_cost' => $resource->to_date_cost ?? 0,
