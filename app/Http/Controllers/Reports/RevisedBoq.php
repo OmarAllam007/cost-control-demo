6673a556db4bd5ac8ad3e79251151b7e9c368c31
omar <omar.coder007@gmail.com>
2017-03-14 16:09:55 +0300

    REvised Boq

--- app/Http/Controllers/Reports/RevisedBoq.php
+++ app/Http/Controllers/Reports/RevisedBoq.php
@@ -80,7 +80,7 @@ AND cost_account=?', [$this->project->id, $level->id, $breakdown->cost_account])
                 }
                 if ($boq && $survey) {
                     if (!isset($tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account])) {
-                        $tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account] = ['cost_account' => $breakdown->cost_account, 'description' => $boq[0]->description, 'revised_boq' => $boq[0]->price_ur * $survey[0]->eng_qty, 'original_boq' => $boq[0]->price_ur * $boq[0]->quantity];
+                        $tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account] = ['cost_account' => $breakdown->cost_account,'description'=>$boq[0]->description, 'revised_boq' => $boq[0]->price_ur * $survey[0]->eng_qty, 'original_boq' => $boq[0]->price_ur * $boq[0]->quantity];
                         $tree['activities'][$activity['activity_id']]['revised_boq'] += $tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account]['revised_boq'];
                         $tree['activities'][$activity['activity_id']]['original_boq'] += $tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account]['original_boq'];
 
@@ -93,43 +93,43 @@ AND cost_account=?', [$this->project->id, $level->id, $breakdown->cost_account])
                 $tree['revised_boq'] += $activity['revised_boq'];
                 $tree['original_boq'] += $activity['original_boq'];
             }
+
+        } else {
+
+            $parent = $level;
+            while ($parent->parent) {
+                $parent = $parent->parent;
+                $boq = Boq::where('wbs_id',$parent->id)->where('project_id',$this->project->id)->where('dry_ur','<>',0)->first();
+                if ($boq) {
+                    foreach ($this->breakdowns->get($level->id) as $breakdown) {
+                        $boq = \DB::select('SELECT price_ur , quantity ,description FROM boqs
+WHERE project_id=?
+AND wbs_id=?
+AND cost_account=?', [$this->project->id, $parent->id, $breakdown->cost_account]);
+
+                        $survey = \DB::select('SELECT eng_qty FROM qty_surveys
+WHERE project_id=?
+AND wbs_level_id=?
+AND cost_account=?', [$this->project->id, $parent->id, $breakdown->cost_account]);
+                        $activity = $this->activities->get($breakdown->id);
+                        if (!isset($tree['activities'][$activity['activity_id']])) {
+                            $tree['activities'][$activity['activity_id']] = ['name' => $activity['activity_name'], 'revised_boq' => 0, 'original_boq' => 0, 'cost_accounts' => []];
+                        }
+                        if ($boq && $survey) {
+                            if (!isset($tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account])) {
+                                $tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account] = ['cost_account' => $breakdown->cost_account,'description'=>$boq[0]->description ,'revised_boq' => $boq[0]->price_ur * $survey[0]->eng_qty, 'original_boq' => $boq[0]->price_ur * $boq[0]->quantity];
+                                $tree['activities'][$activity['activity_id']]['revised_boq'] += $tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account]['revised_boq'];
+                                $tree['activities'][$activity['activity_id']]['original_boq'] += $tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account]['original_boq'];
+                            }
+                        }
+                    }
+                    foreach ($tree['activities'] as $key => $activity) {
+                        $tree['revised_boq'] += $activity['revised_boq'];
+                        $tree['original_boq'] += $activity['original_boq'];
+                    }
+                }
+            }
         }
-//         else {
-//
-//            $parent = $level;
-//            while ($parent->parent) {
-//                $parent = $parent->parent;
-//                $boq = Boq::where('wbs_id',$parent->id)->where('project_id',$this->project->id)->where('dry_ur','<>',0)->first();
-//                if ($boq) {
-//                    foreach ($this->breakdowns->get($level->id) as $breakdown) {
-//                        $boq = \DB::select('SELECT price_ur , quantity ,description FROM boqs
-//WHERE project_id=?
-//AND wbs_id=?
-//AND cost_account=?', [$this->project->id, $parent->id, $breakdown->cost_account]);
-//
-//                        $survey = \DB::select('SELECT eng_qty FROM qty_surveys
-//WHERE project_id=?
-//AND wbs_level_id=?
-//AND cost_account=?', [$this->project->id, $parent->id, $breakdown->cost_account]);
-//                        $activity = $this->activities->get($breakdown->id);
-//                        if (!isset($tree['activities'][$activity['activity_id']])) {
-//                            $tree['activities'][$activity['activity_id']] = ['name' => $activity['activity_name'], 'revised_boq' => 0, 'original_boq' => 0, 'cost_accounts' => []];
-//                        }
-//                        if ($boq && $survey) {
-//                            if (!isset($tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account])) {
-//                                $tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account] = ['cost_account' => $breakdown->cost_account,'description'=>$boq[0]->description ,'revised_boq' => $boq[0]->price_ur * $survey[0]->eng_qty, 'original_boq' => $boq[0]->price_ur * $boq[0]->quantity];
-//                                $tree['activities'][$activity['activity_id']]['revised_boq'] += $tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account]['revised_boq'];
-//                                $tree['activities'][$activity['activity_id']]['original_boq'] += $tree['activities'][$activity['activity_id']]['cost_accounts'][$breakdown->cost_account]['original_boq'];
-//                            }
-//                        }
-//                    }
-//                    foreach ($tree['activities'] as $key => $activity) {
-//                        $tree['revised_boq'] += $activity['revised_boq'];
-//                        $tree['original_boq'] += $activity['original_boq'];
-//                    }
-//                }
-//            }
-//        }
 
         if ($level->children->count()) {
             $tree['children'] = $level->children->map(function (WbsLevel $childLevel) {
