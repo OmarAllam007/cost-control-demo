f21a15340d0439d520a947bdee410e44438ec295
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-04-15 10:07:16 +0300

    Fix login by omar

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,9 @@
 Route::get('/', function () {
     return \Redirect::route('project.index');
 });
-
+if (auth()->id() == 10) {
+    auth()->logout();
+}
 Route::auth();
 Route::get('auth/google', 'Auth\AuthController@googleRedirect');
 Route::get('auth/google/continue', 'Auth\AuthController@googleHandle');
