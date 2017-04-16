a484feae012615d6c2121c09ee3174351fb6f3ea
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-04-16 10:27:44 +0300

    Refactor project info report

--- app/Http/Controllers/Reports/CostReports/ProjectInformation.php
+++ app/Http/Controllers/Reports/CostReports/ProjectInformation.php
@@ -10,42 +10,39 @@ namespace App\Http\Controllers\Reports\CostReports;
 
 
 use App\CostShadow;
+use App\MasterShadow;
+use App\Period;
 use App\Project;
 
 class ProjectInformation
 {
-    function getProjectInformation (Project $project, $period_id)
+    /**
+     * @var Period
+     */
+    private $period;
+
+    function __construct(Period $period)
     {
-        $shadow = \DB::select('SELECT
-  sum(allowable_ev) AS  allowable_cost,
-  sum(to_date_cost)     to_date_cost,
-  sum(cost_var)         cost_var
-FROM (SELECT
+        $this->period = $period;
+    }
 
+    function run()
+    {
+        $data = MasterShadow::forPeriod($this->period)
+            ->selectRaw('sum(allowable_ev_cost) as allowable_cost, sum(to_date_cost) as to_date_cost, sum(allowable_var) as cost_var')
+            ->first()->toArray();
 
-        sum(allowable_ev_cost)  AS allowable_ev,
-        sum(to_date_cost)       AS to_date_cost,
-        sum(cost_var)           AS cost_var
+        if ($data['to_date_cost']) {
+            $data['cpi'] = $data['allowable_cost'] / $data['to_date_cost'];
+        } else {
+            $data['cpi'] = 0;
+        }
 
-      FROM cost_shadows AS cost
-        LEFT JOIN break_down_resource_shadows AS budget ON (cost.breakdown_resource_id = budget.breakdown_resource_id)
-      WHERE cost.project_id = ? AND cost.period_id = (SELECT max(p.period_id)
-                                                       FROM cost_shadows p
-                                                       WHERE p.breakdown_resource_id = cost.breakdown_resource_id) AND cost.period_id <= ?) AS DATA;', [$project->id, $period_id]);
+        $project = $this->period->project;
 
+        $periods = $project->periods()->readyForReporting()->pluck('name', 'id');
 
-        $data = [
-            'actual_cost' => 0,
-            'allowable_cost' => 0,
-            'cpi' => 0,
-            'cost_variance' => 0,
-        ];
-
-        $data['actual_cost'] = $shadow[0]->to_date_cost;
-        $data['allowable_cost'] = $shadow[0]->allowable_cost;
-
-
-        return view('reports.cost-control.project_information', compact('project', 'data', 'allowable_cost', 'actual_cost'));
+        return view('reports.cost-control.project_information', compact('project', 'data', 'periods'));
     }
 
 }
