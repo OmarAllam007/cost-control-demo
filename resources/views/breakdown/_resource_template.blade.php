c540c25a3b4ca14bd0d334deea9e14a0264e6c80
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-05-30 10:00:19 +0300

    Fix out of memory problem while adding a breakdown

--- resources/views/breakdown/_resource_template.blade.php
+++ resources/views/breakdown/_resource_template.blade.php
@@ -1,41 +1,59 @@
 <tr>
     <td>
-        <input class="form-control input-sm" type="hidden" name="resources[{{$index}}][std_activity_resource_id]"
-               id="resourceId{{$index}}" value="{{old("resources.$index.std_activity_resource_id")}}"
-               j-model="std_activity_resource_id" readonly>
-        <input class="form-control input-sm" type="text" name="resources[{{$index}}][resource_type]"
-               id="resourceType{{$index}}" value="{{old("resources.$index.resource_type")}}" j-model="resource_type"
-               readonly>
+        <input class="form-control input-sm" type="hidden"
+               name="resources[{{$index}}][std_activity_resource_id]" id="resourceId{{$index}}"
+               value="{{old("resources.$index.std_activity_resource_id")}}" j-model="std_activity_resource_id" readonly>
+
+        <input class="form-control input-sm"
+               name="resources[{{$index}}][resource_type]" id="resourceType{{$index}}"
+               value="{{old("resources.$index.resource_type")}}" j-model="resource_type" readonly>
     </td>
 
     <td>
-        <input class="form-control input-sm" type="text" name="resources[{{$index}}][resource_name]"
-               id="resourceType{{$index}}" value="{{old("resources.$index.resource_name")}}" j-model="resource_name"
-               readonly>
+        <input class="form-control input-sm"
+               name="resources[{{$index}}][resource_name]" id="resourceType{{$index}}"
+               value="{{old("resources.$index.resource_name")}}" j-model="resource_name"  readonly>
     </td>
 
     <td>
-        <input class="form-control input-sm" type="text" name="resources[{{$index}}][budget_qty]"
-               id="budgetQuantity{{$index}}" value="{{old("resources.$index.budget_qty")}}" j-model="budget_qty">
+        <input class="form-control input-sm"
+               name="resources[{{$index}}][budget_qty]" id="budgetQuantity{{$index}}"
+               value="{{old("resources.$index.budget_qty")}}" j-model="budget_qty" readonly>
     </td>
 
     <td>
-        <input class="form-control input-sm" type="text" name="resources[{{$index}}][eng_qty]"
-               id="engQuantity{{$index}}" value="{{old("resources.$index.eng_qty")}}" j-model="eng_qty">
+        <input class="form-control input-sm"
+               name="resources[{{$index}}][eng_qty]"
+               id="engQuantity{{$index}}"
+               value="{{old("resources.$index.eng_qty")}}" j-model="eng_qty" readonly>
     </td>
 
     <td>
-        <input class="form-control input-sm" type="text" name="resources[{{$index}}][resource_waste]"
-               id="resourceWaste{{$index}}" value="{{old("resources.$index.resource_waste")}}" j-model="resource_waste">
+        <input class="form-control input-sm"
+               name="resources[{{$index}}][resource_waste]"
+               id="resourceWaste{{$index}}" value="{{old("resources.$index.resource_waste")}}"
+               j-model="resource_waste">
     </td>
 
     <td>
-        <input class="form-control input-sm" type="text" name="resources[{{$index}}][labor_count]"
-               id="laborsCount{{$index}}" value="{{old("resources.$index.labor_count")}}" j-model="labor_count">
+        <input class="form-control input-sm"
+               name="resources[{{$index}}][labor_count]"
+               id="laborsCount{{$index}}"
+               value="{{old("resources.$index.labor_count")}}" j-model="labor_count">
     </td>
 
     <td>
-        {{Form::select("resources[$index][productivity_id]", App\Productivity::options(), null, ['class' => 'form-control input-sm', 'j-model' => 'productivity_id', 'id' => "ProductivityID$index"])}}
+        <input class="form-control input-sm" type="hidden"
+               name="resources[{{$index}}][productivity_id]"
+               id="ProductivityID{{$index}}"
+               value="{{old("resources.$index.productivity_id")}}"
+               j-model="productivity_id">
+
+        <input class="form-control input-sm"
+               name="resources[{{$index}}][productivity_ref]"
+               id="ProductivityRef{{$index}}"
+               value="{{old("resources.$index.productivity_ref")}}"
+               j-model="productivity_ref" readonly>
     </td>
 
     <td>
