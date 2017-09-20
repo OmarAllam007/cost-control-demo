e152d44e7ffaad39ddd4369aa54ebf0c55ce39a8
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-09-20 10:46:38 +0300

    Fix resource mapping export for project

--- app/Jobs/Export/ExportResourcesMapping.php
+++ app/Jobs/Export/ExportResourcesMapping.php
@@ -3,16 +3,19 @@
 namespace App\Jobs\Export;
 
 use App\Jobs\Job;
+use App\Project;
 use App\ResourceCode;
+use App\Resources;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
 class ExportResourcesMapping extends Job
 {
+    /** @var Project */
+    protected $project;
 
-    private $project;
-    public function __construct($project)
+    public function __construct(Project $project)
     {
         $this->project = $project;
     }
@@ -25,23 +28,33 @@ class ExportResourcesMapping extends Job
     public function handle()
     {
         $objPHPExcel = new \PHPExcel();
-        $objPHPExcel->setActiveSheetIndex(0);
+        $sheet = $objPHPExcel->getSheet(0);
         set_time_limit(600);
-        $items = ResourceCode::where('project_id', $this->project->id)->get();
 
-        $objPHPExcel->getActiveSheet()->SetCellValue('A1', 'App Code');
-        $objPHPExcel->getActiveSheet()->SetCellValue('B1', 'Store Code');
+        $resources = Resources::where('project_id', $this->project->id)->with('codes')->get();
+
+
+        $sheet->SetCellValue('A1', 'App Code');
+        $sheet->SetCellValue('B1', 'Store Code');
         $rowCount = 2;
 
-        foreach ($items as $item) {
-            $objPHPExcel->getActiveSheet()->SetCellValue('A' . $rowCount, $item->resource->resource_code);
-            $objPHPExcel->getActiveSheet()->SetCellValue('B' . $rowCount, $item->code);
-            $rowCount++;
+        foreach ($resources as $resource) {
+            foreach ($resource->codes as $code) {
+                $sheet->SetCellValue('A' . $rowCount, $resource->resource_code);
+                $sheet->SetCellValue('B' . $rowCount, $code->code);
+                $sheet->SetCellValue('C' . $rowCount, $resource->name);
+                $rowCount++;
+            }
         }
 
+        $sheet->getColumnDimension('A')->setAutoSize(true);
+        $sheet->getColumnDimension('B')->setAutoSize(true);
+        $sheet->getColumnDimension('C')->setAutoSize(true);
+
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="' . $this->project->name . ' - Resources.xlsx"');
+        header('Content-Disposition: attachment;filename="' . slug($this->project->name) . ' - Resources.xlsx"');
         header('Cache-Control: max-age=0');
+
         $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
         $objWriter->save('php://output');
     }
