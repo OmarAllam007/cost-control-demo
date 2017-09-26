df1ba7d7d616cc0ec17968df5d97a0a9b2e4766f
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-09-26 12:54:25 +0300

    Fix revised BOQ report

--- resources/views/reports/budget/high_priority_materials/index.blade.php
+++ resources/views/reports/budget/high_priority_materials/index.blade.php
@@ -20,42 +20,85 @@
 
 @section('body')
 
-    <table class="table table-bordered table-hover">
-        <thead>
-        <tr class="bg-primary">
-            <th class="col-sm-4">Resource Name</th>
-            <th class="col-sm-2">Resource Code</th>
-            <th class="col-sm-2">Budget Unit</th>
-            <th class="col-sm-2">Budget Cost</th>
-            <th class="col-sm-2">Weight (%)</th>
-        </tr>
-        </thead>
-    </table>
-    <section class="vertical-scroll">
-        <table class="table table-bordered table-condensed table-hover" id="report-body">
-            <tbody>
-            @foreach ($tree as $group)
-                <tr class="bg-info">
-                    <th colspan="3">{{$group['name']}}</th>
-                    <th class="text-right">{{number_format($group['budget_cost'], 2)}}</th>
-                    <th>{{number_format($group['weight'], 2)}}%</th>
+    <div class="row">
+        <section class="report-table col-sm-12">
+            <table class="table table-bordered table-hover">
+                <thead>
+                <tr class="bg-primary">
+                    <th class="col-sm-4">Resource Name</th>
+                    <th class="col-sm-2">Resource Code</th>
+                    <th class="col-sm-2">Budget Unit</th>
+                    <th class="col-sm-2">Budget Cost</th>
+                    <th class="col-sm-2">Weight (%)</th>
                 </tr>
+                </thead>
+            </table>
+            <section class="vertical-scroll">
+                <table class="table table-bordered table-condensed table-hover" id="report-body">
+                    <tbody>
+                    @foreach ($tree as $group)
+                        <tr class="bg-info">
+                            <th colspan="3">{{$group['name']}}</th>
+                            <th class="text-right">{{number_format($group['budget_cost'], 2)}}</th>
+                            <th>{{number_format($group['weight'], 2)}}%</th>
+                        </tr>
 
-                @foreach($group['resources'] as $resource)
-                    <tr>
-                        <td class="col-sm-4">{{$resource->name}}</td>
-                        <td class="col-sm-2">{{$resource->resource_code}}</td>
-                        <td class="col-sm-2 text-right">{{number_format($resource->budget_unit, 2)}}</td>
-                        <td class="col-sm-2 text-right">{{number_format($resource->budget_cost, 2)}}</td>
-                        <td class="col-sm-2">{{number_format($resource->weight, 2)}}%</td>
-                    </tr>
-                @endforeach
+                        @foreach($group['resources'] as $resource)
+                            <tr>
+                                <td class="col-sm-4">{{$resource->name}}</td>
+                                <td class="col-sm-2">{{$resource->resource_code}}</td>
+                                <td class="col-sm-2 text-right">{{number_format($resource->budget_unit, 2)}}</td>
+                                <td class="col-sm-2 text-right">{{number_format($resource->budget_cost, 2)}}</td>
+                                <td class="col-sm-2">{{number_format($resource->weight, 2)}}%</td>
+                            </tr>
+                        @endforeach
 
-            @endforeach
-            </tbody>
-        </table>
-    </section>
+                    @endforeach
+                    </tbody>
+                </table>
+            </section>
+        </section>
 
+        <section class="chart-area col-sm-8 col-sm-offset-2">
+            <h4 class="text-center">High priority materials</h4>
+            <div id="high-priority-chart"></div>
+        </section>
+    </div>
+
+
+
+@endsection
+
+@section('javascript')
+    @php
+        $chartLabels = $tree->pluck('name');
+        $chartValues = $tree->pluck('budget_cost')->prepend('Budget Cost');
+    @endphp
+    <script src="/js/d3.min.js"></script>
+    <script src="/js/c3.min.js"></script>
+
+    <script>
+        c3.generate({
+            bindto: '#high-priority-chart',
+            data: {
+                type: 'bar',
+                columns: [{!! $chartValues !!}]
+            },
+            axis: {
+                x: {
+                    type: 'category',
+                    categories: {!! $chartLabels !!}
+                }
+            },
+            grid: {
+                x: {show: true},
+                y: {show: true},
+            },
+            bar: {
+                width: 40
+            }
+        });
+    </script>
 @endsection
 
 @section('css')
@@ -76,5 +119,14 @@
         #report-body tbody tr.highlighted > td {
             background-color: #ffc;
         }
+
+        .chart-area {
+            margin-top: 30px;
+        }
+
+        #high-priority-chart {
+            min-height: 400px;
+        }
     </style>
+    <link rel="stylesheet" href="/css/c3.min.css">
 @endsection
