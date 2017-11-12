3b637b1de29acff40690dd42f5376658b326811d
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-11-12 11:27:21 +0300

    Modify BOQ export process

--- app/Jobs/Export/ExportBoqJob.php
+++ app/Jobs/Export/ExportBoqJob.php
@@ -34,64 +34,33 @@ class ExportBoqJob extends Job
 
     public function handle()
     {
-        set_time_limit(600);
-        $project = $this->project;
         $items = Boq::where('project_id', $this->project->id)->get();
 
         $objPHPExcel = new \PHPExcel();
-        $objPHPExcel->setActiveSheetIndex(0);
-        $objPHPExcel->getActiveSheet()->SetCellValue('A1', 'Code');
-        $objPHPExcel->getActiveSheet()->SetCellValue('B1', 'Cost Account');
-        $objPHPExcel->getActiveSheet()->SetCellValue('C1', 'Description');
-        $objPHPExcel->getActiveSheet()->SetCellValue('D1', 'Discipline');
-        $objPHPExcel->getActiveSheet()->SetCellValue('E1', 'Unit');
-        $objPHPExcel->getActiveSheet()->SetCellValue('F1', 'Estimated Quantity');
-        $objPHPExcel->getActiveSheet()->SetCellValue('G1', 'Unit Price');
-        $objPHPExcel->getActiveSheet()->SetCellValue('H1', 'Unit Dry');
-        $objPHPExcel->getActiveSheet()->SetCellValue('I1', 'KCC-Quantity');
-        $objPHPExcel->getActiveSheet()->SetCellValue('J1', 'Materials');
-        $objPHPExcel->getActiveSheet()->SetCellValue('K1', 'SubContractors');
-        $objPHPExcel->getActiveSheet()->SetCellValue('L1', 'Man Power');
-        $objPHPExcel->getActiveSheet()->SetCellValue('M1', 'WBS-LEVEL');
-//        $objPHPExcel->getActiveSheet()->SetCellValue('N1', 'Division');
-        $objPHPExcel->getActiveSheet()->SetCellValue('N1', 'WBS_PATH');
-        $rowCount = 2;
+        $sheet = $objPHPExcel->getSheet(0);
+        $sheet->fromArray([
+            'Item Code', 'Cost Account', 'Description', 'Discipline', 'Unit', 'Estimated Quantity',
+            'Unit Price', 'Unit Dry', 'KCC-Quantity', 'Materials', 'SubContractors', 'Man Power',
+            'WBS-LEVEL', 'WBS_PATH'],
+            null, 'A1', true);
+
+        $counter = 2;
         foreach ($items as $item) {
-            $code = $item->wbs->code;
-            $objPHPExcel->getActiveSheet()->SetCellValue('A' . $rowCount, $item->item_code);
-            $objPHPExcel->getActiveSheet()->SetCellValue('B' . $rowCount, $item->cost_account);
+            $data = [
+                $item->item_code, $item->cost_account, $item->description, $item->type,
+                $this->units->get($item->unit_id), $item->quantity, $item->price_ur, $item->dry_ur, $item->kcc_qty,
+                $item->materials, $item->subcon, $item->subcon, $this->wbs_levels->get($item->wbs_id)->path,
+                $item->wbs->code
+            ];
 
-            $objPHPExcel->getActiveSheet()->SetCellValue('C' . $rowCount, $item->description);
-
-            $objPHPExcel->getActiveSheet()->SetCellValue('D' . $rowCount, $item->type);
-
-            $objPHPExcel->getActiveSheet()->SetCellValue('E' . $rowCount, $this->units->get($item->unit_id));
-
-            $objPHPExcel->getActiveSheet()->SetCellValue('F' . $rowCount, $item->quantity);
-
-            $objPHPExcel->getActiveSheet()->SetCellValue('G' . $rowCount, $item->price_ur);
-            $objPHPExcel->getActiveSheet()->SetCellValue('H' . $rowCount, $item->dry_ur);
-            $objPHPExcel->getActiveSheet()->SetCellValue('I' . $rowCount, $item->kcc_qty);
-            $objPHPExcel->getActiveSheet()->SetCellValue('J' . $rowCount, $item->materials);
-            $objPHPExcel->getActiveSheet()->SetCellValue('K' . $rowCount, $item->subcon);
-            $objPHPExcel->getActiveSheet()->SetCellValue('L' . $rowCount, $item->subcon);
-
-            $objPHPExcel->getActiveSheet()->SetCellValue('M' . $rowCount, $this->wbs_levels->get($item->wbs_id)->path);
-//            $objPHPExcel->getActiveSheet()->SetCellValue('N' . $rowCount, $this->boqs->get($item->division_id));
-            $objPHPExcel->getActiveSheet()->SetCellValue('N' . $rowCount, $code);
-            $rowCount++;
+            $sheet->fromArray($data, null, "A{$counter}", true);
+            ++$counter;
         }
 
-//        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-//        header('Content-Disposition: attachment;filename="' ."');
-//        header('Cache-Control: max-age=0');
-//        $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
-//        $objWriter->save('php://output');
-
         header('Content-Type: application/vnd.ms-excel');
-        header('Content-Disposition: attachment;filename="'.$project->name.'- BOQ.xlsx"');
+        header('Content-Disposition: attachment;filename="'.$this->project->name.'- BOQ.xlsx"');
         header('Cache-Control: max-age=0');
-        $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel,'Excel5');
+        $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel,'Excel2007');
         $objWriter->save('php://output');
     }
 }
