51d08beaee630dcda071fb9d52430fe02a2efe88
Hazem Mohamed <hazem.mohamed@hubtech.sa>
2017-11-15 12:32:34 +0300

    Show real reason why resources fails in import resources

--- app/Jobs/ResourcesImportJob.php
+++ app/Jobs/ResourcesImportJob.php
@@ -92,7 +92,7 @@ class ResourcesImportJob extends ImportJob
                     Resources::create($item);
                     ++$status['success'];
                 } else {
-                    $data[13] = 'Invalid data';
+                    $data[13] = implode("\n", $validator->errors()->all());
                     $failed->push($data);
                 }
             }
@@ -201,13 +201,13 @@ class ResourcesImportJob extends ImportJob
 
         $sheet->setTitle('Failed resources');
 
-        $sheet->fromArray(["RESOURCE TYPE ", "RESOURCE DIVISION", "RESOURCE SUB DIVISION 1", "RESOURCE SUB DIVISION 2",
+        $sheet->fromArray(["RESOURCE TYPE", "RESOURCE DIVISION", "RESOURCE SUB DIVISION 1", "RESOURCE SUB DIVISION 2",
             "RESOURCE SUB DIVISION 3", "RESOURCE SUB DIVISION 4", "RESOURCE CODE", "RESOURCE NAME", "STANDARD RATE",
-            "UNIT OF MEASURE", "MATERIAL Waste %",	"SUPPLIER/ SUBCON.", "REFERENCE", "REASON"]);
+            "UNIT OF MEASURE", "MATERIAL Waste %",	"SUPPLIER/ SUBCON.", "REFERENCE", "Errors"]);
 
         $failed->each(function($row, $counter) use ($sheet) {
             $row_num = $counter + 2;
-            $sheet->fromArray($row, '', "A{$row_num}");
+            $sheet->fromArray($row, '', "A{$row_num}", true);
         });
 
         $writer = new \PHPExcel_Writer_Excel2007($excel);
