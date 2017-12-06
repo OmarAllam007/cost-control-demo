70ce943ffaab7cefcfbd5d531937e8f5b8772ab2
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-12-06 10:11:50 +0300

    Add admin middleware

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->user()->is_admin) {
+            flash('You are not authorized to do this action');
+            return \Redirect::to('/');
+        }
+
+        return $next($request);
+    }
+}
