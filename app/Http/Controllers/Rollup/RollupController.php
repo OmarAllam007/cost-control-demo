c4060ecc487857cfb3e93c60953b0cbc1a6f926a
Hazem Mohamed <hazem.mohamed@alkifah.com>
2018-01-30 15:14:24 +0300

    Create controller structure

--- app/Http/Controllers/Rollup/RollupController.php
+++ app/Http/Controllers/Rollup/RollupController.php
@@ -10,22 +10,36 @@ class RollupController extends Controller
 {
     function create(Project $project)
     {
+        $this->authorize('cost_owner', $project);
+
         // We get WBS levels from WbsComposer
         return view('rollup.create', compact('project'));
     }
 
     function store(Project $project, Request $request)
     {
+        $this->authorize('cost_owner', $project);
 
+        $rollup = new BreakdownRollup($project, $request->get('cost_accounts', []));
+        $status = $rollup->handle();
+
+        return \Redirect::route('rollup.edit', $project);
     }
 
     function edit(Project $project)
     {
+        $this->authorize('cost_owner', $project);
+
         return view('rollup.edit', compact('project'));
     }
 
     function update(Project $project, Request $request)
     {
+        $this->authorize('cost_owner', $project);
 
+        $rollup = new ImportantResourcesRollup($project, $request->get('cost_accounts', []));
+        $status = $rollup->handle();
+
+        return \Redirect::route('rollup.edit', $project);
     }
 }
