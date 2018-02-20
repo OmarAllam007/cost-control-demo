59355ad179fbe201bd250d1e4985fdf271aedfdf
omar <omar.coder007@gmail.com>
2018-02-20 09:29:37 +0300

    dashboard design

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
 let mix = require('laravel-mix');
 
 mix.sass('resources/assets/sass/app.scss', 'public/css');
-mix.js('resources/assets/js/cost-info-charts.js', 'public/js');
+// mix.js('resources/assets/js/cost-info-charts.js', 'public/js');
