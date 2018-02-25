ceee7cc0d7bf3ed45d74aad9c39590f40ca4745d
Hazem Mohamed <hazem.mohamed@hubtech.sa>
2018-02-25 15:51:44 +0300

    Add validation for rolling up resources level 2

--- resources/assets/js/rollup/components/cost-account.vue
+++ resources/assets/js/rollup/components/cost-account.vue
@@ -14,19 +14,30 @@
 
             <tr class="info">
                 <th class="text-center">
-                    <input type="checkbox" :value="cost_account.id" :name="`cost_account[${cost_account.id}]`">
+                    <input type="checkbox" :value="cost_account.id" :name="`cost_account[${cost_account.id}]`" v-model="selected">
                 </th>
                 <th v-text="cost_account.code"></th>
                 <th v-text="cost_account.description"></th>
-                <th><input class="form-control input-sm" type="text" :value="budget_qty" :name="`budget_unit[${cost_account.id}]`" placeholder="Budget Qty"></th>
-                <th>
-                    <select class="form-control input-sm" :name="`measure_unit[${cost_account.id}]`">
+                <th :class="invalid_budget_qty? 'has-error' : ''">
+                    <input class="form-control input-sm" type="text" :value="budget_qty"
+                           :name="`budget_unit[${cost_account.id}]`"
+                           :required="selected"
+                           placeholder="Budget Qty">
+                </th>
+                <th :class="invalid_measure_unit? 'has-error' : ''">
+                    <select class="form-control input-sm" :name="`measure_unit[${cost_account.id}]`" :required="selected">
                         <option value="">Select Unit</option>
                         <option v-for="unit in units" :value="unit.id" v-text="unit.type"></option>
                     </select>
                 </th>
                 <th v-text="total_budget_cost"></th>
-                <th><input class="form-control input-sm" type="text" :value="0" :name="`to_date_qty[${cost_account.id}]`" placeholder="To date Qty"></th>
+                <th :class="invalid_to_date_qty">
+                    <input class="form-control input-sm"
+                           type="text" :value="0"
+                           :name="`to_date_qty[${cost_account.id}]`"
+                           :required="selected"
+                           placeholder="To date Qty">
+                </th>
                 <th v-text="total_to_date_cost"></th>
             </tr>
             </thead>
@@ -50,7 +61,10 @@
         props: ['initial'],
 
         data() {
-            return { cost_account: this.initial, units: window.units };
+            return {
+                cost_account: this.initial, units: window.units,
+                selected: false
+            };
         },
 
         computed: {
