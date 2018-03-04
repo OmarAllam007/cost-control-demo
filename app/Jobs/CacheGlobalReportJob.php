a13ca458faa41d11a35c15aab2594e2cf9e5714b
Hazem Mohamed <hazem.mohamed@hubtech.sa>
2018-03-04 14:00:07 +0300

    Fix caching of dashboard for periods

--- app/Jobs/CacheGlobalReportJob.php
+++ app/Jobs/CacheGlobalReportJob.php
@@ -2,15 +2,23 @@
 
 namespace App\Jobs;
 
+use App\GlobalPeriod;
 use App\Reports\Cost\GlobalReport;
 use Carbon\Carbon;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
 class CacheGlobalReportJob extends Job implements ShouldQueue
 {
+    private $period;
+
+    public function __construct(GlobalPeriod $period)
+    {
+        $this->period = $period;
+    }
+
     public function handle()
     {
-        $report = new GlobalReport();
+        $report = new GlobalReport($this->period);
 
         \Cache::put('global-report', $report->run(), Carbon::parse('+2 days'));
     }
