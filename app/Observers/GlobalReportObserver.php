947aa3db46ca13cf4ceaaa82853a6d34c760e0c2
Hazem Mohamed <hazem.mohamed@hubtech.sa>
2018-03-04 17:09:06 +0300

    Remove observer for global periods

--- app/Observers/GlobalReportObserver.php
+++ app/Observers/GlobalReportObserver.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hazem
- * Date: 20/12/17
- * Time: 4:47 PM
- */
-
 namespace App\Observers;
-
 
 use App\Jobs\CacheGlobalReportJob;
 
@@ -16,11 +8,11 @@ class GlobalReportObserver
 
     function saved($model)
     {
-        dispatch(new CacheGlobalReportJob());
+//        dispatch(new CacheGlobalReportJob());
     }
 
     function deleted($model)
     {
-        dispatch(new CacheGlobalReportJob());
+//        dispatch(new CacheGlobalReportJob());
     }
 }
