f30ac22905b4bf7bec4b7b6375d6a73455165d9f
Hazem Mohamed <hazem.mohamed@hubtech.sa>
2018-04-09 14:22:40 +0300

    Improve export Qty Survey

--- app/Jobs/Export/ExportSurveyJob.php
+++ app/Jobs/Export/ExportSurveyJob.php
@@ -32,14 +32,14 @@ class ExportSurveyJob extends Job
         $counter = 2;
 
         $sheet->fromArray([
-            'WPS Path', 'WBS Code', 'Item Code', 'Cost Account', 'Description', 'Budget Quantity', 'Engineer Quantity', 'Unit',
+            'WPS Path', 'WBS Code', 'BOQ Item Code', 'QS Item Code', 'Cost Account', 'Description', 'Budget Quantity', 'Engineer Quantity', 'Unit',
             'v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8'
         ], null, "A1", true);
 
         foreach ($this->project->quantities as $qs) {
             $wbs = $this->wbs_levels->get($qs->wbs_level_id);
             $data = [
-                $wbs->path, $wbs->code, $qs->item_code, $qs->cost_account, $qs->description,
+                $wbs->path, $wbs->code, $qs->item_code, $qs->qs_code, $qs->cost_account, $qs->description,
                 $qs->budget_qty, $qs->eng_qty, $this->units->get($qs->unit_id)
             ];
 
@@ -50,6 +50,22 @@ class ExportSurveyJob extends Job
             $sheet->fromArray($data, null, "A{$counter}", true);
             $counter++;
         }
+
+        $autoColumns = ['B', 'C', 'D', 'E', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'];
+        foreach ($autoColumns as $col) {
+            $sheet->getColumnDimension($col)->setAutoSize(true);
+        }
+        $sheet->getColumnDimension('A')->setAutoSize(false)->setWidth(50);
+        $sheet->getColumnDimension('F')->setAutoSize(false)->setWidth(80);
+
+        $sheet->getStyle("A2:E{$counter}")->getNumberFormat()->setFormatCode('@');
+        $sheet->getStyle("F2:G{$counter}")->getNumberFormat()->setBuiltInFormatCode(40);
+        $sheet->getStyle("I2:P{$counter}")->getNumberFormat()->setBuiltInFormatCode(40);
+        $sheet->getStyle("A1:Q1")->applyFromArray([
+            'font' => ['bold' => true], 'fill' => [
+                'type' => 'solid', 'startcolor' => ['rgb' => 'BCDEFA']
+            ]
+        ]);
 
         $filename = storage_path('app/' . uniqid('qs') . '.xlsx');
         \PHPExcel_IOFactory::createWriter($excel, 'Excel2007')->save($filename);
