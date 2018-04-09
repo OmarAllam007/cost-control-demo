4c0011ef24cdff1d29bb599c466cb352f36ad625
Hazem Mohamed <hazem.mohamed@hubtech.sa>
2018-04-09 09:36:11 +0300

    Export activity log to Excel

--- app/Export/ActivityLogExport.php
+++ app/Export/ActivityLogExport.php
@@ -12,8 +12,10 @@ namespace App\Export;
 use App\BreakDownResourceShadow;
 use App\StoreResource;
 use App\WbsLevel;
+use function collect;
 use Illuminate\Support\Collection;
 use PHPExcel_IOFactory;
+use PHPExcel_Worksheet;
 use function response;
 use function storage_path;
 use function str_random;
@@ -73,8 +75,22 @@ class ActivityLogExport
         $this->run();
 
         $excel = PHPExcel_IOFactory::load(storage_path('templates/activity-log.xlsx'));
-        $sheet = $excel->getSheetByName('All Resources');
+        $sheet = $excel->getSheet(1);
+        $this->allResources($sheet);
 
+        $sheet = $excel->getSheet(0);
+        $this->drivingResources($sheet);
+
+
+        $filename = storage_path('app/' . str_random() . '.xlsx');
+        PHPExcel_IOFactory::createWriter($excel, 'Excel2007')->save($filename);
+
+        return response()->download($filename, 'activity-log-' . $this->code . '.xlsx')
+            ->deleteFileAfterSend(true);
+    }
+
+    private function allResources($sheet)
+    {
         $sheet->setCellValue('C2', $this->activity_name);
         $sheet->setCellValue('C3', $this->code);
         $sheet->setCellValue('C4', $this->status);
@@ -110,12 +126,63 @@ class ActivityLogExport
         $sheet->getStyle("B{$start}:Q{$maxRow}")->getBorders()->getAllBorders()->setBorderStyle('thin');
         $sheet->getStyle("B{$start}:Q{$maxRow}")->getBorders()->getOutline()->setBorderStyle('medium');
         $sheet->getStyle("H{$start}:H{$maxRow}")->getBorders()->getRight()->setBorderStyle('medium');
+    }
 
+    private function drivingResources($sheet)
+    {
+        $budget_driving = $this->budget_resources->filter(function($resource) {
+            return $resource->important;
+        })->groupBy('resource_id');
 
-        $filename = storage_path('app/' . str_random() . '.xlsx');
-        PHPExcel_IOFactory::createWriter($excel, 'Excel2007')->save($filename);
+        $resource_ids = $budget_driving->keys();
 
-        return response()->download($filename, 'activity-log-' . $this->code . '.xlsx')
-            ->deleteFileAfterSend(true);
+        $store_driving = $this->store_resources->filter(function($resource) use ($resource_ids) {
+            return $resource_ids->contains($resource->resource_id);
+        })->groupBy('resource_id');
+
+        $this->start = 8;
+        $this->row = 8;
+        foreach ($resource_ids as $id) {
+            $budget_items = $budget_driving->get($id);
+            $store_items = $store_driving->get($id, collect());
+
+            $this->buildResource($sheet, $budget_items, $store_items);
+        }
+
+    }
+
+    /**
+     * @param PHPExcel_Worksheet $sheet
+     * @param Collection         $budget_items
+     * @param Collection         $store_items
+     */
+    private function buildResource($sheet, $budget_items, $store_items)
+    {
+        $budget_row = $start = $this->row;
+
+        foreach ($budget_items as $budget_item) {
+            $sheet->fromArray([
+                $budget_item->resource_code, $budget_item->resource_name, $budget_item->measure_unit,
+                $budget_item->unit_price, $budget_item->budget_unit, $budget_item->budget_cost, $budget_item->cost_account,
+            ], null, "B{$budget_row}", true);
+            ++$budget_row;
+        }
+
+        $store_row = $start;
+        foreach ($store_items as $store_item) {
+            $sheet->fromArray([
+                $store_item->item_code, $store_item->item_desc, $store_item->measure_unit,
+                $store_item->unit_price, $store_item->qty, $store_item->cost, null, null,
+                $store_item->store_date, $store_item->created_at, $store_item->doc_no
+            ], null, "I{$store_row}", true);
+            ++$store_row;
+        }
+
+        $max_row = max($budget_row, $store_row) - 1;
+        $sheet->getStyle("B{$start}:S{$max_row}")->getBorders()->getInside()->setBorderStyle('thin');
+        $sheet->getStyle("B{$start}:H{$max_row}")->getBorders()->getOutline()->setBorderStyle('medium');
+        $sheet->getStyle("I{$start}:S{$max_row}")->getBorders()->getOutline()->setBorderStyle('medium');
+
+        $this->row = ++$max_row;
     }
 }
