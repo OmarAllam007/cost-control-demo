cbd0462dd024b720d2af3e905ff6960147efeb60
Hazem Mohamed <hazem.mohamed@hubtech.sa>
2018-04-12 14:06:15 +0300

    Refactor breakdown templates - wip

--- resources/assets/js/breakdown-templates/templates.vue
+++ resources/assets/js/breakdown-templates/templates.vue
@@ -1,48 +1,60 @@
 <template>
     <section>
-        <section v-if="templates.length">
-        <div class="form-group">
-            <input type="search" class="form-control" v-model="search" placeholder="Search by code or name">
-        </div>
+        <section class="listing">
+            <div v-show="templates.length" class="row">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <input type="search" class="form-control" v-model="search" placeholder="Search by code or name">
+                    </div>
+                </div>
+                <div class="col-md-8 text-right">
+                    <pagination :url="url" property="templates"></pagination>
+                </div>
+            </div>
 
-        <table class="table table-condensed table-striped">
-            <thead>
-            <tr>
-                <th class="col-xs-3">Code</th>
-                <th class="col-xs-6">Template</th>
-                <th class="col-xs-3">Actions</th>
-            </tr>
-            </thead>
+            <div class="loading" v-if="loading">
+                <i class="fa fa-spinner fa-spin fa-3x"></i>
+            </div>
 
-            <tbody>
-            <tr v-for="tpl in templates">
-                <td>
-                    <a :href="`/breakdown-template/${tpl.id}`" v-text="tpl.code"></a>
-                </td>
-                <td>
-                    <a :href="`/breakdown-template/${tpl.id}`" v-text="tpl.name"></a>
-                </td>
-                <td>
-                    <a class="btn btn-sm btn-info" :href="`/breakdown-template/${tpl.id}`">
-                        <i class="fa fa-eye"></i> Show
-                    </a>
+            <table v-if="templates.length" class="table table-condensed table-striped">
+                <thead>
+                <tr>
+                    <th class="col-xs-3">Code</th>
+                    <th class="col-xs-6">Template</th>
+                    <th class="col-xs-3">Actions</th>
+                </tr>
+                </thead>
 
-                    <a v-if="can_edit" class="btn btn-sm btn-primary" :href="`/breakdown-template/${tpl.id}/edit`">
-                        <i class="fa fa-edit"></i> Edit
-                    </a>
-                </td>
-            </tr>
-            </tbody>
-        </table>
+                <tbody>
+                <tr v-for="tpl in templates">
+                    <td>
+                        <a :href="`/breakdown-template/${tpl.id}`" v-text="tpl.code"></a>
+                    </td>
+                    <td>
+                        <a :href="`/breakdown-template/${tpl.id}`" v-text="tpl.name"></a>
+                    </td>
+                    <td>
+                        <a class="btn btn-sm btn-info" :href="`/breakdown-template/${tpl.id}`">
+                            <i class="fa fa-eye"></i> Show
+                        </a>
+
+                        <a v-if="can_edit" class="btn btn-sm btn-primary" :href="`/breakdown-template/${tpl.id}/edit`">
+                            <i class="fa fa-edit"></i> Edit
+                        </a>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <div v-else class="alert alert-info">
+                <i class="fa fa-arrow-left"></i>
+                Please select a division or activity
+            </div>
         </section>
-        <div v-else class="alert alert-info">
-            <i class="fa fa-arrow-left"></i>
-            Please select a division or activity
-        </div>
     </section>
 </template>
 <script>
     import _ from 'lodash';
+    import pagination from '../LaravelPagination.vue';
 
     export default {
         name: 'Templates',
@@ -50,39 +62,52 @@
         props: ['url', 'can_edit'],
 
         data() {
-            return {templates: [], search: ''};
+            return {templates: [], search: '', loading: false};
         },
 
         created() {
-            this.loadTemplates();
+            $(document).ajaxStart(e => {
+                this.loading = true;
+            });
+
+            $(document).ajaxComplete(e => {
+                this.loading = false;
+            });
         },
 
         watch: {
-            url() {
-                this.loadTemplates();
-            },
-
             search() {
                 this.updateSearch();
             }
         },
 
         methods: {
-            loadTemplates() {
-                if (this.url) {
-                    $.ajax({
-                        url: this.url, dataType: 'json'
-                    }).done(response => {
-                        this.templates = response.data
-                    });
-                } else {
-                    this.templates = [];
-                }
-            },
-
-            updateSearch: _.debounce(function() {
+            updateSearch: _.debounce(function () {
                 this.$root.term = this.search;
             }, 500)
+        },
+
+        components: {
+            pagination
         }
     }
 </script>
+
+<style scoped>
+    .listing {
+        position: relative;
+    }
+
+    .loading {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        padding: 100px;
+        background: rgba(255, 255, 255, 0.6);
+    }
+</style>
