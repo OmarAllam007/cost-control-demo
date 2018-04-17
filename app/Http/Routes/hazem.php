38697852a7628b4a9329e0c923ad9548bffcc55a
Hazem Mohamed <hazem.mohamed@hubtech.sa>
2018-04-17 12:49:09 +0300

    Link the button in project to import templates

--- app/Http/Routes/hazem.php
+++ app/Http/Routes/hazem.php
@@ -267,5 +267,5 @@ Route::get('/project/{project}/modify-breakdown/export', 'ModifyBreakdownControl
 Route::get('/dashboard/send', 'DashboardController@send');
 Route::post('/dashboard/send', 'DashboardController@postSend');
 
-Route::get('breakdown-template/import/{project}', 'ImportTemplateToProjectController@create');
-Route::post('breakdown-template/import/{project}', 'ImportTemplateToProjectController@store');
+Route::get('breakdown-template/import-to-project/{project}', 'ImportTemplateToProjectController@create')->name('breakdown-template.import-to-project');
+Route::post('breakdown-template/import-to-project/{project}', 'ImportTemplateToProjectController@store');
