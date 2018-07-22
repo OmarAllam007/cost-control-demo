de261ea1d5e7c99710c9552b0871ea8ecc44aa00
Hazem Mohamed <hazem.mohamed@hubtech.sa>
2018-07-22 13:36:34 +0300

    Calc MR in activity report

--- app/Behaviors/ReportScopes.php
+++ app/Behaviors/ReportScopes.php
@@ -61,7 +61,7 @@ trait ReportScopes
     function scopeCurrentActivityReport(Builder $query, Period $period)
     {
         $query->forPeriod($period);
-        $fields = ['wbs_id', 'activity', 'resource_name'];
+        $fields = ['wbs_id', 'activity', 'activity_id', 'resource_name'];
         $query->groupBy($fields);
         $query->select($fields)->selectRaw(
             'sum(budget_cost) as budget_cost, sum(to_date_cost) to_date_cost, sum(to_date_qty) to_date_qty, sum(allowable_ev_cost) to_date_allowable,'.
